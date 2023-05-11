58e7d28a2d9669b39f5fb99629cc49432db44370
Robin Delbaere <hello@rdelbaere.fr>
2023-05-11 21:23:28 +0200

    Affichage des films

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,9 +11,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class MovieController extends AbstractController
 {
     #[Route('', 'list')]
-    public function list(): Response
+    public function list(MovieRepository $movieRepository): Response
     {
-        return new Response('Page liste des films');
+        $movies = $movieRepository->findAll();
+
+        // TODO
+        // 6 - (bonus) Mettre en forme l'affichage
+
+        return $this->render('movie/list.html.twig', [
+            'movies' => $movies,
+        ]);
     }
 
     #[Route('/{id}', 'view', ['id' => '\d+'])]
