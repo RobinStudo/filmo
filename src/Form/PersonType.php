9ff5ceb34380421ef854060de4b55a6579d1fb1f
Robin Delbaere <hello@rdelbaere.fr>
2023-05-12 20:47:40 +0200

    Configuration du fomulaire personne

--- src/Form/PersonType.php
+++ src/Form/PersonType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Person;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +13,20 @@ class PersonType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('birthdate')
-            ->add('nationality')
+            ->add('firstname', null, [
+                'label' => 'Prénom',
+            ])
+            ->add('lastname', null, [
+                'label' => 'Nom',
+            ])
+            ->add('birthdate', null, [
+                'label' => 'Date de naissance',
+                'widget' => 'single_text'
+            ])
+            ->add('nationality', CountryType::class, [
+                'label' => 'Nationalité',
+                'preferred_choices' => ['FR', 'GB', 'US']
+            ])
         ;
     }
 
