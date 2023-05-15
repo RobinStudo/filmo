de89f9214aa297ef373a4863c964f88f44ac3fd7
Robin Delbaere <hello@rdelbaere.fr>
2023-05-15 16:54:33 +0200

    Fin traitement formulaire personne

--- src/Controller/PersonController.php
+++ src/Controller/PersonController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Person;
 use App\Form\PersonType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,14 +14,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class PersonController extends AbstractController
 {
     #[Route('/nouveau', name: 'create')]
-    public function form(Request $request): Response
+    public function form(EntityManagerInterface $em, Request $request): Response
     {
         $person = new Person();
         $form = $this->createForm(PersonType::class, $person);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            dd($person);
+            $em->persist($person);
+            $em->flush();
+
+            $this->addFlash('notice', 'La personne a bien été créée');
+            return $this->redirectToRoute('person_create');
         }
 
         return $this->render('person/form.html.twig', [
