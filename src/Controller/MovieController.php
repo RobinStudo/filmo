6a94fceb81b92bff4ba0a67c9d6307d9dd3218fc
Robin Delbaere <hello@rdelbaere.fr>
2023-05-15 17:31:03 +0200

    Suppression d'un film

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
 use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -43,8 +45,12 @@ class MovieController extends AbstractController
     }
 
     #[Route('/{id}/supprimer', 'delete', ['id' => '\d+'])]
-    public function delete(): Response
+    public function delete(EntityManagerInterface $em, Movie $movie): Response
     {
-        return new Response('Page de suppression film');
+        $em->remove($movie);
+        $em->flush();
+
+        $this->addFlash('notice', 'Le film a bien été supprimé');
+        return $this->redirectToRoute('movie_list');
     }
 }
