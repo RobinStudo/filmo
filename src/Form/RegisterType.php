761e66b5c6087218f9705d75b0a5a083439f123d
Robin Delbaere <hello@rdelbaere.fr>
2023-05-16 17:02:26 +0200

    Ajout d'une case CGU

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,10 +4,12 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\NotCompromisedPassword;
 use Symfony\Component\Validator\Constraints\Regex;
@@ -25,6 +27,13 @@ class RegisterType extends AbstractType
                 'constraints' => User::getPasswordConstraints(),
             ])
 //            ->add('picture')
+            ->add('acceptTerms', CheckboxType::class, [
+                'label' => 'J\'accepte les conditions du service',
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue(message: 'Vous devez accepter nos conditions !'),
+                ]
+            ])
         ;
     }
 
